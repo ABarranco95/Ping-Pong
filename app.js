f4bf27bce310051a45dee8e9d04f71e74f7f212a
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-11-02 13:26:20 -0800

    [js] Created Canvas as opposed to using vanilla js. Need to create Paddles along with paddle movement

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-const playerOne = document.querySelector('.player1')
-const playerTwo = document.querySelector('.player2')
+let game = document.querySelector('#pong')
 
-console.log(playerOne);
-console.log(playerTwo);
+const ctx = game.getContext('2d')
+
+class Paddle {
+    constructor(x) {
+        this.x = x;
+        this.y = height / 2;
+        this.height = 80;
+        this.width = 10;
+    }
+    createPaddle() {
+        fill(255);
+        rect(this.x, this.y, this.height, this.width)
+    }
+}
+
+const userPaddle = '';
+
+function setup() {
+    userPaddle = new Paddle(20);
+  }
+   
+  function draw() {
+    background(0);
+    userPaddle.createPaddle();
+  }
+
+
+  console.log(userPaddle)
