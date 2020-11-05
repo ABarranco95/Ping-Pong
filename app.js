b6018098de1cbcdabef233ca10f9808724b4d49d
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-11-05 11:12:17 -0800

    {app.js] Added functions for paddles and balls also rendered in

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ context.fillRect(0, 0, game.width, game.height);
 
 context.fillStyle = 'black;'
 
-context.fillStyle = '#fff';
-context.beginPath();
-context.arc(300, 350, 10, 0, Math.PI*2, false);
-context.closePath();
-context.fill();
+// context.fillStyle = '#fff';
+// context.beginPath();
+// context.arc(300, 350, 10, 0, Math.PI*2, false);
+// context.closePath();
+// context.fill();
 
 // Functions that will draw the ball and paddles on board
 
@@ -29,7 +29,6 @@ const ball = {
     color: 'white'
 }
 
-createCircle(ball.x, ball.y, ball.r, ball.color)
 
 function createPaddle(x, y, w, h, color) {
     context.fillStyle = color;
@@ -55,9 +54,6 @@ const aiPaddle = {
     color : 'white'
 }
 
-createPaddle(userPaddle.x, userPaddle.y, userPaddle.width, userPaddle.height, userPaddle.color)
-
-createPaddle(aiPaddle.x, aiPaddle.y, aiPaddle.width, aiPaddle.height, aiPaddle.color)
 
 
 // Add the divider
@@ -70,15 +66,32 @@ const divider = {
     color: 'white',
 }
 
-createPaddle(divider.x, divider.y, divider.width, divider.height, divider.color)
 
 
 // add movement for paddles
 
 document.addEventListener('keydown', function(evt){
     if (evt.key === 'w') {
-        y -= 10
+       userPaddle.y -= 10
     } else if (evt.key === 's') {
-        y += 10
+        userPaddle.y += 10
     }
+
 })
+
+
+function render() {
+    createPaddle(divider.x, divider.y, divider.width, divider.height, divider.color)
+    
+    createPaddle(userPaddle.x, userPaddle.y, userPaddle.width, userPaddle.height, userPaddle.color)
+    
+    createPaddle(aiPaddle.x, aiPaddle.y, aiPaddle.width, aiPaddle.height, aiPaddle.color)
+
+    createCircle(ball.x, ball.y, ball.radius, ball.color)
+}
+
+function pingPong() {
+    render();
+}
+
+setInterval(pingPong, 1000/60) 
