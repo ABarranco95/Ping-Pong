cfd9390c4f13b0fb243d92ada147d19eb56040d6
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-11-09 07:08:42 -0800

    [style.css] Changed colors and styling to make it more friendly and lively. Also now displaying text on board that will eventually be scoreboard

--- app.js
+++ app.js
@@ -10,8 +10,18 @@ const ball = {
     velocityY: 5,
     color: 'white',
 }
+// Function to keep track of score.
 
-// Functions that will draw the ball and paddles on board
+function gameScore(score, x, y, color) {
+    context.fillStyle = color;
+    context.font = '45px Amatic SC';
+    context.fillText(score, x, y);
+}
+
+
+
+
+// Functions that will draw the ball, background, and paddles on board 
 
 function createBackground(x, y, w, h, color) {
     context.fillStyle = color;
@@ -54,9 +64,9 @@ const aiPaddle = {
 // Add the divider
 
 const divider = {
-    x: game.width/2 - 1,
+    x: game.width/2,
     y: 0,
-    width: 0.09,
+    width: 0.8,
     height: game.height,
     color: 'white',
 }
@@ -77,15 +87,16 @@ game.addEventListener('mousemove', movePaddle);
 //Collision Detection
 
 function collision(ball, player) {
+    ball.top = ball.y - ball.radius;
+    ball.bottom = ball.y + ball.radius;
+    ball.left = ball.x - ball.radius;
+    ball.right = ball.x + ball.radius;
+
     player.top = player.y;
     player.bottom = player.y + player.height;
     player.left = player.x;
     player.right = player.x + player.width;
     
-    ball.top = ball.y - ball.radius;
-    ball.bottom = ball.y + ball.radius;
-    ball.left = ball.x - ball.radius;
-    ball.right = ball.x + ball.radius;
     
     return ball.right > player.left && ball.bottom > player.top && ball.left < player.right && ball.top < player.bottom;
 }
@@ -94,7 +105,7 @@ function collision(ball, player) {
 // Run the game on board
 
 function render() {
-    createBackground(0, 0, game.width, game.height, '#000');
+    createBackground(0, 0, game.width, game.height, '#02BDFD');
 
     createPaddle(divider.x, divider.y, divider.width, divider.height, divider.color)
     
@@ -103,6 +114,18 @@ function render() {
     createPaddle(aiPaddle.x, aiPaddle.y, aiPaddle.width, aiPaddle.height, aiPaddle.color)
     
     createCircle(ball.x, ball.y, ball.radius, ball.color)
+
+    gameScore('Play Now!', 300, 300, 'white');
+}
+
+
+// function to reset ball after player/ai scores.
+
+function resetBall() {
+    ball.x = game.width/2;
+    ball.y = game.height/2;
+
+    ball.speed = 4;
 }
 
 
@@ -112,19 +135,33 @@ function update() {
 
     // AI 
 
-    let aiComputer = 0.5
+    let aiComputerLevel = 0.5;
 
-    aiPaddle.y += (ball.y - (aiPaddle.y + aiPaddle.height/2)) * aiComputer;
+    aiPaddle.y += (ball.y - (aiPaddle.y + aiPaddle.height/2)) * aiComputerLevel;
 
     if(ball.y + ball.radius > game.height || ball.y - ball.radius < 0) {
         ball.velocityY = - ball.velocityY;
     }
 
-    let player = (ball.x < game.width/2) ? user: aiComputer;
+    let player = (ball.x < game.width/2) ? userPaddle: aiPaddle;
 
     if(collision(ball, player)) {
+        
+        let collidePoint = ball.y - (player.y + player.height/2);
+
+        collidePoint = collidePoint/(player.height/2);
+
+        let angle = collidePoint * Math.PI/4;
+
+        let direction = (ball.x < game.width/2) ? 1 : -1;
+
+        ball.velocityX = direction * ball.speed * Math.cos(angle);
+        ball.velocityY = ball.speed * Math.sin(angle);
+
+        ball.speed += 1;
 
     }
+
 }
 
 function pingPong() {
